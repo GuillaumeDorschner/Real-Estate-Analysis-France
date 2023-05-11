034691f534140df15cddd99066eb23baad6be205
Louis ANNE <louis.anne@edu.devinci.fr>
2023-05-11 22:46:58 +0200

    Auto stash before merge of "html" and "origin/html"

--- valeur_fonciere/views.py
+++ valeur_fonciere/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .analyse.test import analyse_data
 import os
+import pandas as pd
 
 def index(request):
     return render(request, "index.html")
@@ -14,7 +15,7 @@ def analyse(request):
     pages = []
     for filename in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, filename)):
-            pages.append(filename)
+            pages.append(filename.split('.')[0])
             print(filename)
 
     print(pages)
@@ -31,3 +32,14 @@ def analyse(request):
 
 def about(request):
     return render(request, "about.html")
+
+def analyse_annee(request, annee):
+    if os.path.isfile('./data/valeursfoncieres-'+annee+'.txt'):
+        f = open('./data/valeursfoncieres-'+annee+'.txt', 'r')
+        data = f.read()
+    context = {
+        "var":data
+    }
+
+    template = loader.get_template("analyse/index.html")
+    return HttpResponse(template.render(context, request))
