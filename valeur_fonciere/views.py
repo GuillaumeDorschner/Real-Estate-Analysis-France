889e2e6c5f1de0f879546891fa2158e2103572ef
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2023-05-16 11:55:38 +0200

    loading ajax js on client side

--- valeur_fonciere/views.py
+++ valeur_fonciere/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .analyse.graph import *
+import time
 import os
 import pandas as pd
+from django.http import JsonResponse
 
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
 
 def analyse(request):
 
@@ -25,36 +30,47 @@ def analyse(request):
     template = loader.get_template("analyse/index.html")
     return HttpResponse(template.render(context, request))
 
-
-def about(request):
-    return render(request, "about.html")
-
 def analyse_intra(request, annee):
-    if os.path.isfile('./data/annee_traitee/'+annee+'.csv'):
-        df = pd.read_csv('./data/annee_traitee/'+annee+'.csv',sep=';',header=0)
-        context = {
-            "graph": graph_region('regions',df),
-            "Vente_mois" : Vente_par_Mois(df),
-            "topPlusMoinsCher" : topPlusMoinsCher(df),
-        }
-    else :
-        context = {"var":"Pas de données pour cette année"}
+    # if os.path.isfile('./data/annee_traitee/'+annee+'.csv'):
+    #     df = pd.read_csv('./data/annee_traitee/'+annee+'.csv',sep=';',header=0)
+    #     context = {
+    #         "graph": graph_region('regions',df),
+    #         "Vente_mois" : Vente_par_Mois(df),
+    #         "topPlusMoinsCher" : topPlusMoinsCher(df),
+    #     }
+    # else :
+    #     context = {"var":"Pas de données pour cette année"}
+    # template = loader.get_template("analyse/template_intra.html")
+    # return HttpResponse(template.render(context, request))
+
+    print(annee)
+    context = {
+        "annee": annee
+    }
 
     template = loader.get_template("analyse/template_intra.html")
     return HttpResponse(template.render(context, request))
 
 
-def analyse_inter(request, annee):
+def analyse_inter(request):
+#     if os.path.isfile('./data/annee_traitee/'+annee+'.csv'):
+#         df = pd.read_csv('./data/annee_traitee/'+annee+'.csv',sep=';',header=0)
+#         context = {
+#             "graph": graph_region('regions',df),
+#             "Vente_mois" : Vente_par_Mois(df),
+#             "topPlusMoinsCher" : topPlusMoinsCher(df),
+#         }
+#     else :
+#         context = {"var":"Pas de données pour cette année"}
+
+#     template = loader.get_template("analyse/template_inter.html")
+#     return HttpResponse(template.render(context, request))
+    return None
+
+def get_graph(request, annee, graph, filtre):
     if os.path.isfile('./data/annee_traitee/'+annee+'.csv'):
         df = pd.read_csv('./data/annee_traitee/'+annee+'.csv',sep=';',header=0)
-        context = {
-            "graph": graph_region('regions',df),
-            "Vente_mois" : Vente_par_Mois(df),
-            "topPlusMoinsCher" : topPlusMoinsCher(df),
-        }
-    else :
-        context = {"var":"Pas de données pour cette année"}
-
-    template = loader.get_template("analyse/template_inter.html")
-    return HttpResponse(template.render(context, request))
-
+        if graph == 'graph_region' and filtre == 'regions':
+            graph_url = graph_region(filtre,df)
+            return JsonResponse({'graph_url': graph_url})
+    return JsonResponse({'graph_url': 'none'})
