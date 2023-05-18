62d3c12d8941a241d2896ea745026e3d9eff7361
Louis ANNE <louis.anne@edu.devinci.fr>
2023-05-19 00:48:52 +0200

    la pelle est lancée

--- valeur_fonciere/analyse/graph.py
+++ valeur_fonciere/analyse/graph.py
@@ -77,7 +77,7 @@ def top_5_moins_cher(request, df):
 
     return HttpResponse(html_fig)
 
-def vol_monetaire(df):
+def vol_monetaire(request,df):
     """Volume monétaire par département
     graph fixe"""
     DEPARTMENTS = json.load(open("../data/departements/departements_dict.json")) 
@@ -96,7 +96,7 @@ def vol_monetaire(df):
     image_html = fig.to_html()
     return JsonResponse({"Vol_monetaire": image_html})
 
-def prix_m2(m2):
+def prix_m2(request,df):
     """Calcul du prix moyen au m2 par département
     graph non interactif"""
     df['Valeur fonciere par m2'] = df['Valeur fonciere'] / df['Surface terrain']
@@ -185,3 +185,125 @@ def nb_ventes(request, df):
     fig_html = fig.to_html()
     return JsonResponse({"nb_ventes": fig_html})
 
+def evo_m2(request,data):
+    temp = data[(data["Type local"] != "Dépendance")& (data["Type local"] != "Local industriel. commercial ou assimilé")].reset_index(drop = True)
+    temp["Month mutation"] = temp["Date mutation"].dt.month
+
+    temp['Prix mètre carré Paris 2018'] = temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2018)]["Valeur fonciere"]/temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2018)]["Surface reelle bati"]
+    temp['Prix mètre carré Paris 2019'] = temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2019)]["Valeur fonciere"]/temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2019)]["Surface reelle bati"]
+    temp['Prix mètre carré Paris 2022'] = temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2022)]["Valeur fonciere"]/temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2022)]["Surface reelle bati"]
+
+    temp =temp.replace(np.inf, np.nan)
+
+    # temp['No. of Recovered to 1 Death Case'] = round(temp['Recovered']/temp['Deaths'], 3)
+    temp = temp.groupby('Month mutation')["Prix mètre carré Paris 2018","Prix mètre carré Paris 2019","Prix mètre carré Paris 2022"].mean().reset_index()
+
+    temp = temp.melt(id_vars='Month mutation', value_vars=['Prix mètre carré Paris 2018','Prix mètre carré Paris 2019','Prix mètre carré Paris 2022'], 
+                    var_name='Departements', value_name='Value')
+
+    fig = px.line(temp, x="Month mutation", y="Value", color='Departements', log_y=True, 
+                title='Evolution du prix du mètre carré à paris depuis 2018', color_discrete_sequence=[dth, rec,act])
+    retour = mpld3.fig_to_html(plt.gcf())
+    plt.close()
+    return HttpResponse(retour)
+
+def nb_ventes_par_mois(request, data):
+    """Nombre de ventes par mois
+    graph fixe"""
+    df = pd.DataFrame(data)
+    df["Date mutation"] =pd.to_datetime(df['Date mutation'],dayfirst=True).dt.strftime('%y-%m')
+    plt.figure(figsize=(20,10))
+    plt.title('Nombre de ventes répartis par mois')
+    plt.plot(df["Date mutation"].value_counts()[df['Date mutation'].unique()])
+    plt.xticks(rotation=45)
+    plt.xlabel('Date')
+    plt.ylabel('Nombre de ventes')
+    plt.show()
+    html_fig = mpld3.fig_to_html(plt.gcf())
+    plt.close()
+    return HttpResponse(html_fig)
+
+def evo_m_Carrez (request,df):
+    dth = '#ff2e63' 
+    rec = '#21bf73'
+    act = '#fe9801'
+    temp = df[(df["Type local"] != "Dépendance")& (df["Type local"] != "Local industriel. commercial ou assimilé")].reset_index(drop = True)
+    temp["Month mutation"] = temp["Date mutation"].dt.month
+    temp["carrez_sum"] = temp["Surface Carrez du 1er lot"].fillna(0)  +  temp["Surface Carrez du 2eme lot"].fillna(0) + temp["Surface Carrez du 3eme lot"].fillna(0) + temp["Surface Carrez du 4eme lot"].fillna(0) + temp["Surface Carrez du 5eme lot"].fillna(0)
+    temp["Prix mètre carré"] = np.where(temp["carrez_sum"] != 0,temp["Valeur fonciere"]/temp["carrez_sum"],temp["Valeur fonciere"]/temp["Surface reelle bati"])
+    temp = temp.drop(np.where(temp['Prix mètre carré'] > 25000)[0])
+
+    temp['Prix mètre carré Paris 2018'] = temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2018)]["Prix mètre carré"]
+    temp['Prix mètre carré Paris 2019'] = temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2019)]["Prix mètre carré"]
+    temp['Prix mètre carré Paris 2022'] = temp[(temp["Code departement"] == '75')&(temp["Date mutation"].dt.year == 2022)]["Prix mètre carré"]
+
+    temp =temp.replace(np.inf, np.nan)
+
+    # temp['No. of Recovered to 1 Death Case'] = round(temp['Recovered']/temp['Deaths'], 3)
+    temp = temp.groupby('Month mutation')["Prix mètre carré Paris 2018","Prix mètre carré Paris 2019","Prix mètre carré Paris 2022"].mean().reset_index()
+
+    temp = temp.melt(id_vars='Month mutation', value_vars=['Prix mètre carré Paris 2018','Prix mètre carré Paris 2019','Prix mètre carré Paris 2022'], 
+                    var_name='Departements', value_name='Value')
+
+    fig = px.line(temp, x="Month mutation", y="Value", color='Departements', log_y=True, 
+                title='Evolution du prix du mètre carré à paris depuis 2018', color_discrete_sequence=[dth, rec,act])
+    fig_html = mpld3.fig_to_html(plt.gcf())
+    return HttpResponse(fig_html)
+
+def location(row):
+        if row['Code departement']=='75':
+                return 'Paris'
+        elif row['Code departement']=='6':
+                return 'Alpes-Maritimes'
+        elif row['Code departement']=='59':
+            return 'Nord'
+        else:
+            return 'Reste de la France'
+        
+
+def graph_dynamique1(request,data):
+    dth = '#ff2e63' 
+    rec = '#21bf73'
+    carrez = data[(data["Type local"] != "Dépendance")& (data["Type local"] != "Local industriel. commercial ou assimilé")].reset_index(drop = True)
+    carrez["carrez_sum"] = data["Surface Carrez du 1er lot"].fillna(0)  +  data["Surface Carrez du 2eme lot"].fillna(0) + data["Surface Carrez du 3eme lot"].fillna(0) + data["Surface Carrez du 4eme lot"].fillna(0) + data["Surface Carrez du 5eme lot"].fillna(0)
+    carrez["Prix mètre carré"] = np.where(carrez["carrez_sum"] != 0,carrez["Valeur fonciere"]/carrez["carrez_sum"],carrez["Valeur fonciere"]/carrez["Surface reelle bati"])
+    carrez = carrez.drop(np.where(carrez['Prix mètre carré'] > 25000)[0])
+
+    
+
+
+    temp = carrez.copy()
+    temp['Region'] = temp.apply(location, axis=1)
+    temp['Date'] = temp['Date mutation'].dt.strftime('%Y-%m-%d')
+    temp = temp.groupby(['Region', 'Date'])['Prix mètre carré'].mean().reset_index()
+    temp = temp.melt(id_vars=['Region', 'Date'], value_vars=['Prix mètre carré'], 
+                    var_name='Case', value_name='Count').sort_values('Count')
+    # temp = temp.sort_values(['Date', 'Region', 'Case']).reset_index()
+    temp.head()
+
+    fig = px.bar(temp, y='Region', x='Count', color='Case', barmode='group', orientation='h',
+                text='Count', title='Hubei - China - World', animation_frame='Date',
+                color_discrete_sequence= [dth, rec, cnf], range_x=[0, 15000])
+    fig.update_traces(textposition='outside')
+    # fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    # fig.update_layout(yaxis={'categoryorder':'array', 
+    #                          'categoryarray':['Hubei','Other Chinese Provinces','Rest of the World']})
+    fig_html = mpld3.fig_to_html(plt.gcf())
+    return HttpResponse(fig_html)
+
+def graoh_dynamique2(request,carrez):
+    cnf = '#393e46'
+    temp = carrez.copy()
+    temp['Region'] = temp.apply(location, axis=1)
+    temp = temp.groupby('Region')['Prix mètre carré'].mean().reset_index()
+    temp = temp.melt(id_vars='Region', value_vars=['Prix mètre carré'], 
+                    var_name='Case', value_name='Count').sort_values('Count')
+    temp.head()
+
+    fig = px.bar(temp, y='Region', x='Count', color='Case', barmode='group', orientation='h',
+                text='Count', title='Paris - Nord - Alpes-Maritimes', 
+                color_discrete_sequence= [cnf])
+    fig.update_traces(textposition='outside')
+    #fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    fig_html = mpld3.fig_to_html(plt.gcf())
+    return HttpResponse(fig_html)
